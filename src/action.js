3a139a15d8f3b77073034651daa0ce66420c2f71
mr-Dev-Anshu <anshuies31@gmail.com>
2024-06-12 19:04:39 +0530

    changes in signup

--- src/action.js
+++ src/action.js
@@ -55,6 +55,12 @@ export const signUp = async (formData) => {
   }
 
   dbconnection();
+
+    const check = await User.find({userid}) ; 
+
+    if(check) {
+       throw new Error("Already Signed  in ,  Please Login ") ; 
+    }
   const hashedPassword = await bcryptjs.hash(password, 10);
   const newUser = await User.create({ userid, password: hashedPassword });
   console.log(newUser);
--- src/app/allimages/page.js
+++ src/app/allimages/page.js
@@ -2,21 +2,28 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-const page =  () => {
+const page = () => {
   const [allImage, setAllImage] = useState();
   const getData = async () => {
     try {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getimages`);
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/getimages`,
+        {
+          headers: {
+            "Cache-Control": "no-store",
+          },
+        }
+      );
       console.log(res.data);
-      setAllImage(res.data) ; 
+      setAllImage(res.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-   useEffect(()=> {
-      getData()
-   } , []) ; 
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="text-white md:px-6 md:py-8 space-y-4 ">
